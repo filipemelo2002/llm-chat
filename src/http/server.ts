52177d7ea8143fa89d04f9c290cbdc38aa8320e8
Filipe Melo <filipemelo032@gmail.com>
2025-06-15 13:15:23 -0300

    chore: create logger

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,6 @@
+import { logger } from '@utils/logger.utils'
 import app from './app'
 
 app.listen(8080, () => {
-  console.log("Server running  at 8080!")
+  logger.info("Server running  at 8080!")
 })
