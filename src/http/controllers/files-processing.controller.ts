558d42d15f4b771807494d326ef49cc67f060070
Filipe Melo <filipemelo032@gmail.com>
2025-06-15 13:14:54 -0300

    feat: implement queue embeding usecase to process file in background

--- src/http/controllers/files-processing.controller.ts
+++ src/http/controllers/files-processing.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import { UploadFileUseCase } from "@application/use-cases/uploadFile.usecase";
+import { QueueEmbedingProcessUseCase } from "@application/use-cases/queueEmbedingProcess.usecase";
 
 export class FilesProcessingController {
   constructor(
-    private uploadFileUseCase: UploadFileUseCase = new UploadFileUseCase()
+    private uploadFileUseCase: UploadFileUseCase = new UploadFileUseCase(),
+    private  queueEmbedingProcessUseCase = new QueueEmbedingProcessUseCase()
   ) {}
   async processFile(request: Request, response: Response) {
     const file = request.file;
@@ -15,17 +17,18 @@ export class FilesProcessingController {
     }
 
     try {
-      await this.uploadFileUseCase.execute(file);
+      const {fileName} = await this.uploadFileUseCase.execute(file);
+      const id = await this.queueEmbedingProcessUseCase.execute({filename: fileName});
+      response.status(200).json({
+        message: "File uploaded successfully",
+        processId: id
+      });
+    return;
     } catch (exception) {
       response.status(500).json({
         message: "Error when processing file. Try again later.",
       });
       process.exit(-1);
     }
-
-    response.status(200).json({
-      message: "File uploaded successfully",
-    });
-    return;
   }
 }
